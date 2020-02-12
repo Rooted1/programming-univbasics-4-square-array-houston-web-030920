edf985f543de19d66df7d59d2f66873651faa4b7
Ruth Obe <ruthobey@gmail.com>
2020-02-12 21:09:43 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -6,7 +6,7 @@ def square_array(array)
   
   while array[counter] do
     puts "Exponent:"
-    new_numbers.push(array[counter] ** array[counter])
+    new_numbers.push(array[counter] ** 2 )
     counter += 1
   end 
 end
